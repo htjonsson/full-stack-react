796cdeb6c0ed035fff7c6a5cb2b963a208799c70
Hordur <hordur.jonsson@spikeglobal.com>
2023-05-10 17:15:50 +0100

    Create a backend C# WebApi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 import { Layout, Space } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Sider, Content } = Layout;
 import Employee from './components/employee/Employee';
 import Table from './components/table/table';
-import TableColumn from './components/table-column/table-column'
-import SideMenu from './SideMenu'
+import TableColumn from './components/table-column/table-column';
+import SideMenu from './components/side-menu';
 import './App.css'
 
 function App () {
   const [menuKey, setMenuKey] = useState(null);
+  const [collapsed, setCollapsed] = useState(true);
 
   const handleMenuAction = (e) => {
     setMenuKey(e.key);
@@ -25,7 +26,7 @@ function App () {
         <Layout>
             <Header className='headerStyle'>Header</Header>
             <Layout>
-                <Sider width={200} className='siderStyle' style={{backgroundColor:'white'}}>
+                <Sider width={200} className='siderStyle' style={{backgroundColor:'white'}} collapsible collapsed={collapsed}>
                     <SideMenu handleMenuAction={handleMenuAction}/>
                 </Sider>
                 <Content className='contentStyle'>
