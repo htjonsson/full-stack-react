1799ca20440ec614b68b65ef33022eafe83e5527
Hordur <hordur.jonsson@spikeglobal.com>
2023-07-05 16:01:11 +0100

    Create a simple erd diagrams

--- backend/MorphCore/MorphCore.CodeGenerator/ImageBuilder.cs
+++ backend/MorphCore/MorphCore.CodeGenerator/ImageBuilder.cs
@@ -44,6 +44,88 @@ public class ImageBuilder
         }
     }
 
+
+    private void DrawSeparator(int x, int y, int width, int height)
+    {
+        this.Builder.AppendLine($"<rect x=\"{x}\" y=\"{y}\" width=\"{width}\" height=\"{height}\" style=\"fill:rgb(254,245,219);\" />");
+    
+        // left line        
+        this.Builder.AppendLine($"<line x1=\"{x}\" y1=\"{y}\" x2=\"{x}\" y2=\"{y+height}\" style=\"stroke:rgb(228,220,197);stroke-width:1;\" />");
+
+        // right line
+        this.Builder.AppendLine($"<line x1=\"{x+width}\" y1=\"{y}\" x2=\"{x+width}\" y2=\"{y+height}\" style=\"stroke:rgb(228,220,197);stroke-width:1;\" />");  
+
+        // right line
+        this.Builder.AppendLine($"<line x1=\"{x}\" y1=\"{y+(height/2)}\" x2=\"{x+width}\" y2=\"{y+(height/2)}\" style=\"stroke:rgb(228,220,197);stroke-width:1;\" />");      
+    }
+
+    private void DrawHorizonalLine(int x, int y, int width)
+    {
+         this.Builder.AppendLine($"<line x1=\"{x}\" y1=\"{y}\" x2=\"{x+width}\" y2=\"{y}\" style=\"stroke:rgb(228,220,197);stroke-width:1;\" />");       
+    }
+
+    private void DrawFrame(int x, int y, int width, int height)
+    {
+        this.Builder.AppendLine($"<rect x=\"{x}\" y=\"{y}\" width=\"{width}\" height=\"{height}\" style=\"fill:rgb(254,245,219);\" />");
+    
+        // left line        
+        this.Builder.AppendLine($"<line x1=\"{x}\" y1=\"{y}\" x2=\"{x}\" y2=\"{y+height}\" style=\"stroke:rgb(228,220,197);stroke-width:1;\" />");
+
+        // right line
+        this.Builder.AppendLine($"<line x1=\"{x+width}\" y1=\"{y}\" x2=\"{x+width}\" y2=\"{y+height}\" style=\"stroke:rgb(228,220,197);stroke-width:1;\" />");      
+    }
+
+    private void DrawBlueFrame(int x, int y, int width, int height)
+    {
+        this.Builder.AppendLine($"<rect x=\"{x}\" y=\"{y}\" width=\"{width}\" height=\"{height}\" style=\"fill:white;stroke:rgb(48,159,219);stroke-width:1;\" />");
+    }
+
+    private void DrawText(int x, int y, bool isPrimary, bool isForeign, bool isRequired, string name, string dataType)
+    {          
+        x += 4;
+        // Primary Key
+        if (isPrimary)
+            this.Builder.AppendLine($"<text x=\"{x}\" y=\"{y}\" font-family=\"Roboto\" font-size=\"7pt\" >P</text>"); 
+
+        x += 8;
+        // Foreign Key
+        if (isForeign)
+            this.Builder.AppendLine($"<text x=\"{x}\" y=\"{y}\" font-family=\"Roboto\" font-size=\"7pt\" >F</text>"); 
+        
+        x += 8;
+        // Required
+        if (isRequired)
+            this.Builder.AppendLine($"<text x=\"{x}\" y=\"{y}\" fill=\"red\" font-family=\"Roboto\" font-size=\"7pt\" >*</text>"); 
+        
+        x += 18;
+        // Name
+        if (string.IsNullOrWhiteSpace(name) == false)
+            this.Builder.AppendLine($"<text x=\"{x}\" y=\"{y}\" font-family=\"Roboto\" font-size=\"7pt\" >{name}</text>"); 
+        
+        x += 210;
+        // Type
+        if (string.IsNullOrWhiteSpace(dataType) == false)
+            this.Builder.AppendLine($"<text x=\"{x}\" y=\"{y}\" font-family=\"Roboto\" font-size=\"7pt\" fill=\"rgb(152,147,160)\" >{dataType}</text>");          
+    }  
+
+    private void DrawCenteredText(int x, int y, string text)
+    {
+        if (string.IsNullOrWhiteSpace(text) == false)
+        {
+            this.Builder.AppendLine($"<text x=\"{x}\" y=\"{y}\" font-family=\"Roboto\" font-size=\"7pt\" font-weight=\"bold\" text-anchor=\"middle\">{text}</text>");  
+        }          
+    }
+
+    private void DrawBox(int x, int y, int width, int height) 
+    {
+        this.Builder.AppendLine($"<rect x=\"{x}\" y=\"{y}\" width=\"{width}\" height=\"{height}\" style=\"fill:rgb(254,245,219);stroke:rgb(228,220,197);stroke-width:1;\"/>");
+    }
+
+    private void DrawDot(int x, int y)
+    {
+        this.Builder.AppendLine($"<circle cx=\"{x}\" cy=\"{y}\" r=\"1\" />");
+    }
+
     private void Title(int x, int y, int width, int height, string text)
     {
         this.Builder.AppendLine($"<rect x=\"{x}\" y=\"{y}\" width=\"{width}\" height=\"{height}\" style=\"fill:rgb(254,245,219);stroke:rgb(228,220,197);stroke-width:1;\"/>");
@@ -91,51 +173,42 @@ public class ImageBuilder
         return 20 + 8 + count * 16;
     }
 
-    public override string ToString(SvgTable table)
+    public string ToString(int width, int height, List<ModelNode> nodes)
     {
-        BeginSvg(800, 800);
+        int baseline = 10;
+        int titleBaseline = baseline + 5;
 
-        int x = 50;
-        int y = 100;
-        int width = GetWidth();
-        int height = GetHeight(table.Columns.Count);
+        BeginSvg(width, height);
 
-        Frame(x - 3, y - 3, width + 6, height + 6, false);
+        nodes.ForEach(n => {
+            var r = n.Rect.Inflate(3);
+            DrawBlueFrame(r.X, r.Y, r.Width, r.Height);
 
-        Frame(x, y, width, height);
-
-        Title(x, y, width, 20, table.Title);
-
-        y += 20 + 8 + 8;
-        foreach(var column in table.Columns)
-        {
-            Column(x + 4, y, column.IsPrimary, column.IsForeign, column.isRequired, column.Name, column.DataType);
-            y += 16;
-        }
+            n.Items.ForEach(i => {
+                if (i.Type == ModelItemType.Text) 
+                {
+                    DrawFrame(i.Rect.X, i.Rect.Y, i.Rect.Width, i.Rect.Height);
+                    DrawText(i.Rect.X, i.Rect.Y + baseline, i.IsPrimary, i.IsForeign, i.IsRequired, i.Text, i.Extent);
+                }
+                else if (i.Type == ModelItemType.Separator) 
+                {
+                    DrawSeparator(i.Rect.X, i.Rect.Y, i.Rect.Width, i.Rect.Height);
+                }
+                else if (i.Type == ModelItemType.HorizontalLine)
+                {
+                    DrawHorizonalLine(i.Rect.X, i.Rect.Y, i.Rect.Width);
+                }
+                else if (i.Type == ModelItemType.Title)
+                {
+                    DrawFrame(i.Rect.X, i.Rect.Y, i.Rect.Width, i.Rect.Height);
+                    DrawHorizonalLine(i.Rect.X, i.Rect.Y, i.Rect.Width);
+                    DrawCenteredText(i.Rect.X + (i.Rect.Width / 2), i.Rect.Y + titleBaseline, i.Text);
+                }
+            });     
+        });
 
         EndSvg();
 
         return this.Builder.ToString();
-    }
+    }       
 }
-
-public class SvgTable 
-{
-    public string Title { get; set; } = string.Empty;
-    public int Height { get; set; }
-    public int Width { get; set; }
-    public List<SvgColumn> Columns { get; set; } = new List<SvgColumn>();
-}
-
-public class SvgColumn
-{
-    public string Name { get; set; } = string.Empty;
-    public string DataType { get; set; } = string.Empty;
-    public bool IsPrimary { get; set; } = false;
-    public bool IsForeign { get; set; } = false;
-    public bool isRequired { get; set; } = false;    
-}
-
-
-
-
--- backend/MorphCore/MorphCore.CodeGenerator/Program.cs
+++ backend/MorphCore/MorphCore.CodeGenerator/Program.cs
@@ -43,36 +43,73 @@ string connectionString = @"URI=file:northwind.sqlite3";
 string version = SqliteUtils.GetVersion(connectionString) ?? String.Empty;
 Console.WriteLine($"SQLite version: {version}");
 
+var tables = SqliteUtils.GetTables(connectionString);
 
+if (false) 
+{
+    tables.ForEach(t => {
+        Console.WriteLine($"{t.Name.ToUpper()}");
+        Console.WriteLine("---COLUMNS---");  
 
-var tables = SqliteUtils.GetTableNames(connectionString);
-tables.ForEach(t => {
-    Console.WriteLine($"{t.ToUpper()}");
-    Console.WriteLine("---COLUMNS---");
-
-    var table = new SvgTable()
-    {
-        Title = t
-    };
-
-    var columns = SqliteUtils.GetColumns(connectionString, $"{x}");
-    columns.ForEach(c => {
-        Console.WriteLine($"{c.Name.ToUpper()} {c.Type} {c.Defaults}");
-
-        var column = new SvgColumn()
-        {
-            Name = c.Name,
-            DataType = c.Type,
-            IsPrimary = (c.Key > 0 ? true : false),
-            isRequired = (c.IsNull ? false : true)
-        };
-        table.Columns.Add(column);
+        t.Columns.ForEach(c => {
+            Console.WriteLine($"{c.Name.ToUpper()} {c.Type} {c.Defaults}");
+        });
+        Console.WriteLine("--------------------------------------------------");
     });
-    Console.WriteLine("--------------------------------------------------");
+}
+
+// ----------------------------------------------------------------------------------------------------------------
+//      NODES
+// ----------------------------------------------------------------------------------------------------------------
+
+var nodes = new List<ModelNode>();
+
+if (true) 
+{
+    tables.ForEach(t => {
+        var node = new ModelNode();
+
+        node.AddTitle(t.Name.ToUpper());
+        node.AddSeparator();
+        
+        t.Columns.ForEach(c => {
+            var name = c.Name.ToUpper();
+            var extent = c.Type;
+            var isPrimary = (c.Key > 0 ? true : false);
+            var isForeign = false;
+            var isRequired = (c.IsNull == true ? false : true);
+
+            node.AddText(name, extent, isPrimary, isForeign, isRequired);            
+        });
+
+        if(t.Columns.Count > 0)
+        {
+            node.AddHorizontalLine();
+        }
+
+        nodes.Add(node);
+    });
+}
+
+// ----------------------------------------------------------------------------------------------------------------
+//      LAYOUT CALCULATIONS
+// ----------------------------------------------------------------------------------------------------------------
+
+// Set default width and height
+nodes.ForEach(n => {
+    n.SetDefaults();
+});
+
+int x = 80;
+int y = 10;
+nodes.ForEach(n => {
+    var rect = n.SetLocation(x, y);
+    
+    y = rect.Y + rect.Height + 20 + 2;
 });
 
 // ----------------------------------------------------------------------------------------------------------------
-//      SVG
+//      DRAWING SVG
 // ----------------------------------------------------------------------------------------------------------------
 
 var builder = new ImageBuilder();
@@ -80,9 +117,190 @@ var builder = new ImageBuilder();
 string path = "output.svg";
 
 File.Delete(path);
-File.WriteAllText(path, builder.ToString());
+File.WriteAllText(path, builder.ToString(600, 3000, nodes));
 
 // ---------------------------------------------------------------------------------------------------------------- 
 //      END OF CONSOLE APPLICATION
 // ----------------------------------------------------------------------------------------------------------------
 // Console.ReadLine();
+
+public class Location 
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+public class Rect
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public Rect()
+    {
+        this.X = this.Y = this.Width = this.Height = 0;
+    }
+
+    public Rect(int x, int y, int width, int height)
+    {
+        this.X = x;
+        this.Y = y;
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public Rect Inflate(int factor)
+    {
+        var r = new Rect();
+
+        r.X = this.X - factor;
+        r.Y = this.Y - factor;
+        r.Width = this.Width + factor*2;
+        r.Height = this.Height + factor*2;
+
+        return r;
+    }
+}
+
+public class ModelNode 
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = String.Empty;
+    public Rect Rect { get; set; } = new Rect();
+    public List<ModelItem> Items { get; set; } = new List<ModelItem>();
+
+    public void SetDefaults()
+    {
+      Items.ForEach(i => {
+        i.Rect.Width = 380;
+
+        if (i.Type == ModelItemType.Text) 
+        {
+            i.Rect.Height = 16;
+        }
+        else if (i.Type == ModelItemType.Separator) 
+        {
+            i.Rect.Height = 5;
+        }
+        else if (i.Type == ModelItemType.HorizontalLine)
+        {
+            i.Rect.Height = 1;
+        }
+        else if (i.Type == ModelItemType.Title)
+        {
+            i.Rect.Height = 20;
+        }
+    });      
+    }
+
+    public Rect SetLocation(int x, int y)
+    {
+        this.Rect.X = x;
+        this.Rect.Y = y;
+        this.Rect.Height = GetHeight();
+        this.Rect.Width = GetWidth();
+
+        this.Items.ForEach(item => {
+            item.Rect.X = x;
+            item.Rect.Y = y;
+
+            y += item.Rect.Height;
+        });
+
+        return this.Rect;
+    }
+
+    public int GetHeight()
+    {
+        int height = 0;
+
+        Items.ForEach(x => {
+            height += x.Rect.Height;
+        });
+        return height;
+    }
+
+    public int GetWidth()
+    {
+        int width = Int32.MinValue;
+
+        Items.ForEach(x => {
+            width = Int32.Max(width, x.Rect.Width);
+        });
+        return width;
+    }
+
+    public void AddSeparator()
+    {
+        this.Items.Add(new ModelItem() {
+            Type = ModelItemType.Separator
+        });
+    }
+
+    public void AddHorizontalLine()
+    {
+        this.Items.Add(new ModelItem() {
+            Type = ModelItemType.HorizontalLine
+        });
+    }
+
+    public void AddText(string text, string extent, bool isPrimary, bool isForeign, bool isRequired)
+    {
+        this.Items.Add(new ModelItem() {
+            Type = ModelItemType.Text,
+            Text = text,
+            Extent = extent,
+            IsPrimary = isPrimary,
+            IsForeign = isForeign,
+            IsRequired = isRequired
+        });
+    }
+
+    public void AddTitle(string text)
+    {
+          this.Items.Add(new ModelItem() {
+            Type = ModelItemType.Title,
+            Text = text
+        });      
+    }
+}
+
+public enum ModelItemType 
+{
+    None,
+    Text,
+    Title,
+    Separator,
+    HorizontalLine
+}
+
+public class ModelItem 
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Rect Rect { get; set; } = new Rect();
+
+    public Location GetRightAncor()
+    {
+        return new Location() {
+            X = Rect.X + Rect.Width,
+            Y = Rect.Y + (Rect.Height / 2)
+        };
+    }
+
+    public Location GetLeftAncor()
+    {
+        return new Location() {
+            X = Rect.X,
+            Y = Rect.Y + (Rect.Height / 2)
+        };
+    }    
+
+    public ModelItemType Type { get; set; } = ModelItemType.None;
+
+    public bool IsRequired { get; set; } = false;
+    public bool IsPrimary { get; set; } = false;
+    public bool IsForeign { get; set; } = false;
+    public string Text { get; set; } = String.Empty;
+    public string Extent { get; set; } = String.Empty;
+}
